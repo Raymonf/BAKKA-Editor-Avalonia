1c6e197a54333962933c4ac356f4fb44d78896b3
Raymonf <raymonf@outlook.com>
2023-02-17 02:28:42 -0500

    upgrade to Avalonia 11.0.0-preview5

--- BAKKA_Editor/ViewLocator.cs
+++ BAKKA_Editor/ViewLocator.cs
@@ -7,11 +7,8 @@ namespace BAKKA_Editor;
 
 public class ViewLocator : IDataTemplate
 {
-    public IControl? Build(object? data)
+    public Control Build(object data)
     {
-        if (data is null)
-            return null;
-
         var name = data.GetType().FullName!.Replace("ViewModel", "View");
         var type = Type.GetType(name);
 
@@ -20,10 +17,10 @@ public class ViewLocator : IDataTemplate
             return (Control)Activator.CreateInstance(type)!;
         }
         
-        return new TextBlock { Text = name };
+        return new TextBlock { Text = "Not Found: " + name };
     }
 
-    public bool Match(object? data)
+    public bool Match(object data)
     {
         return data is ViewModelBase;
     }
