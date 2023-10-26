cecf49d56802d37198892f493a08d3814429e97d
Raymonf <raymonf@outlook.com>
2023-10-26 01:33:09 -0400

    Add some slight documentation for L()
    ...might be a bad idea extending object? just a little?

--- BAKKA_Editor/Controls/ControlExtensions.cs
+++ BAKKA_Editor/Controls/ControlExtensions.cs
@@ -5,6 +5,12 @@ namespace BAKKA_Editor.Controls;
 
 public static class ControlExtensions
 {
+    /// <summary>
+    /// Get localized string from application resources
+    /// </summary>
+    /// <param name="control">The current object</param>
+    /// <param name="key">The key to pull from resources</param>
+    /// <returns>A string or null</returns>
     public static string? L(this object control, string key)
     {
         var resource = Application.Current?.FindResource(key);
