cdc6bb3f60894a232105c023d56d7c3345f42b08
Yasu3D <64372170+Yasu3D@users.noreply.github.com>
2023-11-04 20:37:24 +0100

    fixed errors in bake hold algorithm (#27)
    * fixed errors in bake hold algorithm
    
    woopsie
    
    * we do a little more fixing
    
    oopsie part 2
    
    * Reformat + move locks out
    
    ---------
    
    Co-authored-by: Raymonf <raymonf@outlook.com>

--- BAKKA_Editor/BakeHold.cs
+++ BAKKA_Editor/BakeHold.cs
@@ -13,9 +13,10 @@ namespace BAKKA_Editor
 {
     internal class BakeHold
     {
-        public static void StepSymmetric(Chart chart, Note startNote, Note endNote, float length, int positionChange, int sizeChange, OperationManager operationManager)
+        public static void StepSymmetric(Chart chart, Note startNote, Note endNote, float length, int positionChange,
+            int sizeChange, OperationManager operationManager)
         {
-            decimal interval = (decimal)(1 / (1 / length * Math.Abs(positionChange)));
+            decimal interval = (decimal) (1 / (1 / length * Math.Abs(positionChange)));
             int positionStep = (Math.Abs(positionChange) > Math.Abs(sizeChange) ? 2 : 1) * Math.Sign(positionChange);
             int sizeStep = (Math.Abs(positionChange) > Math.Abs(sizeChange) ? 1 : 2) * Math.Sign(sizeChange);
 
@@ -25,46 +26,56 @@ namespace BAKKA_Editor
             var lastNote = startNote;
             List<Note> segmentList = new List<Note>();
 
-            for (decimal i = (decimal)startNote.Measure + interval; i < (decimal)endNote.Measure; i += interval)
+            lock (chart)
             {
-                newPosition += positionStep;
-                newSize += sizeStep;
-
-                var newNote = new Note()
+                for (decimal i = (decimal) startNote.Measure + interval; i < (decimal) endNote.Measure; i += interval)
                 {
-                    BeatInfo = new((float)i),
-                    NoteType = NoteType.HoldJoint,
-                    Position = newPosition,
-                    Size = newSize,
-                    HoldChange = true,
-                    PrevReferencedNote = lastNote,
-                    NextReferencedNote = endNote
-                };
+                    // avoid decimal/floating point errors that would
+                    // otherwise cause two segments on the same beat
+                    // if i is just *barely* less than endNote.Measure.
+                    if (chart.GetBeat((double) i) == chart.GetBeat(endNote.Measure))
+                        break;
 
-                lastNote.NextReferencedNote = newNote;
-                endNote.PrevReferencedNote = newNote;
+                    newPosition += positionStep;
+                    newSize += sizeStep;
 
-                // this is pure brainfuck, let me explain.
-                // give new note reference to last placed note. will be startNote if the loop just started.
-                // give last note reference to new note.
-                // give new note reference to end note. this will be overwritten by every iteration to always keep the connections correct.
-                // give end note reference to new note. ^same as above.
+                    var newNote = new Note()
+                    {
+                        BeatInfo = new((float) i),
+                        NoteType = NoteType.HoldJoint,
+                        Position = newPosition,
+                        Size = newSize,
+                        HoldChange = true,
+                        PrevReferencedNote = lastNote,
+                        NextReferencedNote = endNote
+                    };
 
-                lastNote = newNote;
-                segmentList.Add(newNote);
+                    lastNote.NextReferencedNote = newNote;
+                    endNote.PrevReferencedNote = newNote;
 
-                lock (chart) chart.Notes.Add(newNote);
-                chart.IsSaved = false;
+                    // this may be pure brainfuck, let me explain.
+                    // give new note reference to last placed note. will be startNote if the loop just started.
+                    // give new note reference to end note.
+                    // give last note reference to new note. this will be overwritten by every iteration to always keep the connections correct.
+                    // give end note reference to new note. same as above ^
+
+                    lastNote = newNote;
+                    segmentList.Add(newNote);
+
+                    chart.Notes.Add(newNote);
+                    chart.IsSaved = false;
+                }
             }
 
             operationManager.Push(new BakeHoldNote(chart, startNote, endNote, segmentList));
         }
 
-        public static void StepAsymmetric(Chart chart, Note startNote, Note endNote, float length, int positionChange, int sizeChange, OperationManager operationManager)
+        public static void StepAsymmetric(Chart chart, Note startNote, Note endNote, float length, int positionChange,
+            int sizeChange, OperationManager operationManager)
         {
-            decimal interval = (decimal)(1 / (1 / length * Math.Max(Math.Abs(positionChange), Math.Abs(sizeChange))));
-            int positionStep = Math.Sign(positionChange);
-            int sizeStep = Math.Sign(sizeChange);
+            decimal interval = (decimal) (1 / (1 / length * Math.Max(Math.Abs(positionChange), Math.Abs(sizeChange))));
+            int positionStep = int.Sign(positionChange);
+            int sizeStep = int.Sign(sizeChange);
 
             int newPosition = startNote.Position;
             int newSize = startNote.Size;
@@ -72,88 +83,107 @@ namespace BAKKA_Editor
             var lastNote = startNote;
             List<Note> segmentList = new List<Note>();
 
-            for (decimal i = (decimal)startNote.Measure + interval; i < (decimal)endNote.Measure; i += interval)
+            lock (chart)
             {
-                newPosition += positionStep;
-                newSize += sizeStep;
-
-                var newNote = new Note()
+                for (decimal i = (decimal) startNote.Measure + interval; i < (decimal) endNote.Measure; i += interval)
                 {
-                    BeatInfo = new((float)i),
-                    NoteType = NoteType.HoldJoint,
-                    Position = newPosition,
-                    Size = newSize,
-                    HoldChange = true,
-                    PrevReferencedNote = lastNote,
-                    NextReferencedNote = endNote
-                };
+                    if (chart.GetBeat((double) i) == chart.GetBeat(endNote.Measure))
+                        break;
 
-                lastNote.NextReferencedNote = newNote;
-                endNote.PrevReferencedNote = newNote;
+                    newPosition += positionStep;
+                    newSize += sizeStep;
 
-                lastNote = newNote;
-                segmentList.Add(newNote);
+                    var newNote = new Note()
+                    {
+                        BeatInfo = new((float) i),
+                        NoteType = NoteType.HoldJoint,
+                        Position = newPosition,
+                        Size = newSize,
+                        HoldChange = true,
+                        PrevReferencedNote = lastNote,
+                        NextReferencedNote = endNote
+                    };
 
-                lock (chart) chart.Notes.Add(newNote);
-                chart.IsSaved = false;
+                    lastNote.NextReferencedNote = newNote;
+                    endNote.PrevReferencedNote = newNote;
+
+                    lastNote = newNote;
+                    segmentList.Add(newNote);
+
+                    chart.Notes.Add(newNote);
+                    chart.IsSaved = false;
+                }
             }
 
             operationManager.Push(new BakeHoldNote(chart, startNote, endNote, segmentList));
         }
 
-        public static void LerpRound(Chart chart, Note startNote, Note endNote, float length, int positionChange, int sizeChange, OperationManager operationManager)
+        public static void LerpRound(Chart chart, Note startNote, Note endNote, int positionChange,
+            OperationManager operationManager)
         {
             decimal interval = 0.015625m;
 
             int newPosition = startNote.Position;
             int newSize = startNote.Size;
+            int direction = int.Sign(positionChange);
 
             var lastNote = startNote;
             List<Note> segmentList = new List<Note>();
 
-            for (decimal i = (decimal)startNote.Measure + interval; i < (decimal)endNote.Measure; i += interval)
+            lock (chart)
             {
-                float lerpTime = ((float)i - startNote.Measure) / (endNote.Measure - startNote.Measure);
-
-                var virtualPosStart0 = startNote.Position;
-                var virtualPosEnd0 = endNote.Position;
-
-                var virtualPosStart1 = startNote.Position + startNote.Size;
-                var virtualPosEnd1 = endNote.Position + endNote.Size;
-
-
-                newPosition = (int)MathF.Round(Lerp(virtualPosStart0, virtualPosEnd0, lerpTime));
-                newSize = (int)MathF.Round(Lerp(virtualPosStart1, virtualPosEnd1, lerpTime)) - newPosition;
-
-                var newNote = new Note()
+                for (decimal i = (decimal) startNote.Measure + interval; i < (decimal) endNote.Measure; i += interval)
                 {
-                    BeatInfo = new((float)i),
-                    NoteType = NoteType.HoldJoint,
-                    Position = newPosition,
-                    Size = newSize,
-                    HoldChange = false,
-                    PrevReferencedNote = lastNote,
-                    NextReferencedNote = endNote
+                    if (chart.GetBeat((double) i) == chart.GetBeat(endNote.Measure))
+                        break;
 
-                };
+                    float lerpTime = ((float) i - startNote.Measure) / (endNote.Measure - startNote.Measure);
 
-                lastNote.NextReferencedNote = newNote;
-                endNote.PrevReferencedNote = newNote;
+                    var virtualPosStart0 = startNote.Position;
+                    var virtualPosEnd0 = endNote.Position;
 
-                lastNote = newNote;
-                segmentList.Add(newNote);
+                    var virtualPosStart1 = startNote.Position + startNote.Size;
+                    var virtualPosEnd1 = endNote.Position + endNote.Size;
 
-                lock (chart) chart.Notes.Add(newNote);
-                chart.IsSaved = false;
+                    newPosition = (int) MathF.Round(ShortLerp(direction, virtualPosStart0, virtualPosEnd0, lerpTime));
+                    newSize = (int) MathF.Round(ShortLerp(direction, virtualPosStart1, virtualPosEnd1, lerpTime)) -
+                              newPosition;
 
-                System.Diagnostics.Debug.WriteLine(newPosition + ", " + newSize + ", " + i);
+                    var newNote = new Note()
+                    {
+                        BeatInfo = new((float) i),
+                        NoteType = NoteType.HoldJoint,
+                        Position = (newPosition + 60) % 60,
+                        Size = newSize,
+                        HoldChange = false,
+                        PrevReferencedNote = lastNote,
+                        NextReferencedNote = endNote
+                    };
+
+                    lastNote.NextReferencedNote = newNote;
+                    endNote.PrevReferencedNote = newNote;
+
+                    lastNote = newNote;
+                    segmentList.Add(newNote);
+
+                    chart.Notes.Add(newNote);
+                    chart.IsSaved = false;
+
+                    // System.Diagnostics.Debug.WriteLine(newPosition + ", " + newSize + ", " + i);
+                }
             }
 
             operationManager.Push(new BakeHoldNote(chart, startNote, endNote, segmentList));
         }
 
-        private static float Lerp(int a, int b, float t)
+        private static float ShortLerp(int direction, int a, int b, float t)
         {
+            if (direction > 0 && a > b)
+                a -= 60;
+
+            if (direction < 0 && a < b)
+                a += 60;
+
             return (1 - t) * a + t * b;
         }
     }
