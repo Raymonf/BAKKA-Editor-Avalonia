afd81087d4dccc553ebf449d2ace7cdb2c39bd6f
Raymonf <raymonf@outlook.com>
2023-11-05 20:24:21 -0500

    vroom vroom

---
+++ BAKKA_Editor/ScaledMeasurePositionInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace BAKKA_Editor;
+
+public class ScaledMeasurePositionInfo
+{
+    public float GimmickStartMeasure { get; set; }
+    public float PartialScaledPosition { get; set; }
+    public float LastMeasurePosition { get; set; }
+    public float CurrentHiSpeedValue { get; set; }
+    public float CurrentTimeSigValue { get; set; }
+}
